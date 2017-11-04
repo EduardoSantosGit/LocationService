bbe6e4e1007dbe0e8adc0ce355cf153f020d7323
EduardoSantosGit <eduardosantos058@gmail.com>
2017-11-04 20:49:50 -0200

    init

---
+++ LocationService/Configure/InjectionDependency.cs
@@ -0,0 +1,26 @@
+﻿using LightInject;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationService.Configure
+{
+    public class InjectionDependency
+    {
+        public static ServiceContainer ConfigureService()
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            var containerOptions = new ContainerOptions { EnablePropertyInjection = false };
+            var container = new ServiceContainer(containerOptions);
+
+
+
+            container.ScopeManagerProvider = new PerLogicalCallContextScopeManagerProvider();
+
+            return container;
+        }
+    }
+}
---
+++ LocationService/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using LocationService.Configure;
+using LightInject.Microsoft.DependencyInjection;
+
+namespace LocationService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().AddControllersAsServices().AddJsonFormatters();
+
+            var container = InjectionDependency.ConfigureService();
+            return container.CreateServiceProvider(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
