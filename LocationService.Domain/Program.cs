bcdb6f0a0475e0500d79bdeba48f7afe4425bd27
EduardoSantosGit <eduardosantos058@gmail.com>
2017-11-04 21:37:46 -0200

    add service request api infra

---
+++ LocationService.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocationService.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- LocationService/Configure/InjectionDependency.cs
+++ LocationService/Configure/InjectionDependency.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LocationService.Configure
+namespace LocationService.Api.Configure
 {
     public class InjectionDependency
     {
