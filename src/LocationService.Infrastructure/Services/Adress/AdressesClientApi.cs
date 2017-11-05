09d213a4eb02742115f1d09f9dd5c3b8cef8b099
EduardoSantosGit <eduardosantos058@gmail.com>
2017-11-04 22:08:58 -0200

    implement organization project init tests

---
+++ src/LocationService.Infrastructure/Services/Adress/AdressesClientApi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationService.Infrastructure.Services.Adress
+{
+    public class AdressesClientApi
+    {
+        public readonly HttpClient _httpClient;
+        public readonly string baseUrl;
+        public readonly string apiUrl;
+
+        public AdressesClientApi(HttpClient httpClient = null)
+        {
+            _httpClient = httpClient ?? new HttpClient();
+            _httpClient.Timeout = TimeSpan.FromSeconds(30);
+            baseUrl = "http://www.buscacep.correios.com.br/";
+            apiUrl = "sistemas/buscacep/resultadoBuscaCepEndereco.cfm";
+        }
+
+        public async Task<string> GetAdressesCep(string zipCode)
+        {
+            var nvc = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("relaxation", zipCode),
+                new KeyValuePair<string, string>("tipoCEP", "ALL"),
+                new KeyValuePair<string, string>("semelhante", "N")
+            };
+
+            var request = new HttpRequestMessage(HttpMethod.Post,
+                baseUrl + apiUrl) { Content = new FormUrlEncodedContent(nvc) };
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return response.Content.ReadAsStringAsync().Result;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ test/LocationService.InfrastructureTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocationService.IntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
