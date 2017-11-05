4ad29e1cfe9a3b4c8abb9213fab681fab2f9b642
EduardoSantosGit <eduardosantos058@gmail.com>
2017-11-05 21:43:20 -0200

    implement test and method verify scrap

---
+++ test/LocationService.IntegrationTest/Adress/AdressesServiceScrapIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using LocationService.Infrastructure.Services.Adresses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LocationService.IntegrationTest.Adress
+{
+    public class AdressesServiceScrapIntegrationTest
+    {
+        public AdressesServiceScrap CreateInstance()
+        {
+            return new AdressesServiceScrap();
+        }
+
+        [Fact]
+        public async Task GetAdressesPage_WhenHtmlData_ReturnsAdress()
+        {
+            var adressApi = new AdressesClientApi();
+            var result = await adressApi.GetAdressesCep("01311200");
+
+            var scrap = CreateInstance();
+            var adress = scrap.GetAdressesPage(result);
+
+            Assert.NotNull(adress);  
+        }
+    }
+}
