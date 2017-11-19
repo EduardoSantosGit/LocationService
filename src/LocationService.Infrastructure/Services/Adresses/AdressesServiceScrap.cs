95720dec5e67dc28e42666711ba785d273048150
EduardoSantosGit <eduardosantos058@gmail.com>
2017-11-19 21:52:31 -0200

    implement role scrap list table duo

--- src/LocationService.Infrastructure/Services/Adresses/AdressesServiceScrap.cs
+++ src/LocationService.Infrastructure/Services/Adresses/AdressesServiceScrap.cs
@@ -39,20 +39,57 @@ namespace LocationService.Infrastructure.Services.Adresses
             var lines = table.Split("<tr");
 
             var listAdress = new List<Adress>();
+            var listAdressDouble = new List<Adress>();
 
-            for(var i=2;i< lines.Length; i++)
+            for (var i = 2; i < lines.Length; i++)
             {
                 var columns = lines[i].Split("<td");
 
-                var adress = new Adress
-                {
-                    Street = _scrapParser.ScrapBlockPage(columns[1], "\">", "</td>")?.Trim(),
-                    District = _scrapParser.ScrapBlockPage(columns[2], "\">", "</td>")?.Trim(),
-                    Locality = _scrapParser.ScrapBlockPage(columns[3], "\">", "</td>")?.Trim(),
-                    ZipCode = _scrapParser.ScrapBlockPage(columns[4], "\">", "</td>")?.Trim()
-                };
+                var col = _scrapParser.ScrapBlockPage(columns[1], "\">", "</td>")?.Trim();
 
-                listAdress.Add(adress);
+                if (col.Contains("<br"))
+                {
+                    var duplicate = col.Split("<br>");
+
+                    for (var j = 0; j < duplicate.Length; j++)
+                    {
+                        if (!string.IsNullOrEmpty(duplicate[j]))
+                        {
+                            var street = default(string);
+                            if (duplicate[j].Contains("</a>") || duplicate[j].Contains(">"))
+                            {
+                                street = duplicate[j]?.Replace("</a>", "")?.Trim();
+                            }
+                            else
+                            {
+                                street = duplicate[j]?.Trim();
+                            }
+
+                            listAdress.Add(new Adress
+                            {
+                                Street = street,
+                                District = _scrapParser.ScrapBlockPage(columns[2], "\">", "</td>")?.Trim(),
+                                Locality = _scrapParser.ScrapBlockPage(columns[3], "\">", "</td>")?.Trim(),
+                                ZipCode = _scrapParser.ScrapBlockPage(columns[4], "\">", "</td>")?.Trim()
+                            });
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    var adress = new Adress
+                    {
+                        Street = _scrapParser.ScrapBlockPage(columns[1], "\">", "</td>")?.Trim(),
+                        District = _scrapParser.ScrapBlockPage(columns[2], "\">", "</td>")?.Trim(),
+                        Locality = _scrapParser.ScrapBlockPage(columns[3], "\">", "</td>")?.Trim(),
+                        ZipCode = _scrapParser.ScrapBlockPage(columns[4], "\">", "</td>")?.Trim()
+                    };
+
+                    listAdress.Add(adress);
+                }
+                
             }
 
             return listAdress;
