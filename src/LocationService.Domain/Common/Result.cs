198127819fff3332a91d30698a8c3acea9d12e41
EduardoSantosGit <eduardosantos058@gmail.com>
2018-03-16 23:08:47 -0300

    enum result

--- src/LocationService.Domain/Common/Result.cs
+++ src/LocationService.Domain/Common/Result.cs
@@ -4,8 +4,29 @@ using System.Text;
 
 namespace LocationService.Domain.Common
 {
+    
+    public enum ResultStatusCode
+    {
+        UnprocessableEntity = -12,
+        NotImplemented = -11,
+        InvalidPage = -10,
+        InvalidSession = -9,
+        ProxyBlocked = -8,
+        BadCaptcha = -7,
+        Unavailable = -6,
+        TimedOut = -5,
+        BadRequest = -4,
+        Duplicated = -3,
+        NotFound = -2,
+        Error = -1,
+        OK = 1,
+        None = 0
+    }
+
     public class Result<T>
     {
+
+
 
     }
 }
