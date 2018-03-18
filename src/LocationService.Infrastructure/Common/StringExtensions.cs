441e7e47e3f2ebc5a330bf1c5babe6e5e4a5ec10
EduardoSantosGit <eduardosantos058@gmail.com>
2018-03-17 21:01:24 -0300

    add string extensions split

---
+++ src/LocationService.Infrastructure/Common/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationService.Infrastructure.Common
+{
+    public static class StringExtensions
+    {
+
+        public static string[] SplitString(this string value, string separating)
+        {
+            string[] array = { separating };
+            return value.Split(array, System.StringSplitOptions.RemoveEmptyEntries);
+        }
+
+    }
+}
