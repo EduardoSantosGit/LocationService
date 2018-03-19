beb9a3be75742fb8c7979e88ee3d5ca057596191
EduardoSantosGit <eduardosantos058@gmail.com>
2018-03-18 21:09:14 -0300

    remove methods role provider

--- src/LocationService.Infrastructure/Services/Adresses/AdressesService.cs
+++ src/LocationService.Infrastructure/Services/Adresses/AdressesService.cs
@@ -9,29 +9,17 @@ using System.Threading.Tasks;
 
 namespace LocationService.Infrastructure.Services.Adresses
 {
-    public class AdressesMailService : IAddressProvider
+    public class AdressesMailService : IAdressesServices
     {
         public readonly ClientMailApi _adressesClientApi;
-        public readonly AdressesServiceScrap _adressesServiceScrap;
-        public readonly ScrapParser _scrapParser;
 
-        public AdressesMailService(ClientMailApi adressesClientApi)
+        public AdressesMailService()
         {
-            _adressesClientApi = adressesClientApi;
-            _adressesServiceScrap = new AdressesServiceScrap();
-            _scrapParser = new ScrapParser();
         }
 
         public async Task<Adress> GetAdressesZipCode(string zipCode)
         {
-            var html = await _adressesClientApi.PostSendAsync(zipCode);
-
-            var valid = _scrapParser.ContainsValue(html, "<p>DADOS ENCONTRADOS COM SUCESSO.</p>", true);
-
-            if (valid)
-            {
-                return _adressesServiceScrap.GetAdressesPageCode(html);
-            }
+           
             
             return null;
         }
@@ -39,14 +27,7 @@ namespace LocationService.Infrastructure.Services.Adresses
         public async Task<List<Adress>> GetAdressesTerm(string term)
         {
 
-            var html = await _adressesClientApi.PostSendAsync(term);
-
-            var valid = _scrapParser.ContainsValue(html, "<p>DADOS ENCONTRADOS COM SUCESSO.</p>", true);
-
-            if (valid)
-            {
-                return _adressesServiceScrap.GetAdressesPageTerm(html);
-            }
+            
 
             return null;
         }
