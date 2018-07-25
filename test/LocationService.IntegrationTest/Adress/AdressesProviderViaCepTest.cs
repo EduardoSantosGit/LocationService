c49759baf5e2241e7d1a940df97d5d9952d74925
eduardosantosgit <eduardosantos058@gmail.com>
2018-07-25 13:27:50 -0300

    add test verify data json field

--- test/LocationService.IntegrationTest/Adress/AdressesProviderViaCepTest.cs
+++ test/LocationService.IntegrationTest/Adress/AdressesProviderViaCepTest.cs
@@ -11,11 +11,30 @@ namespace LocationService.IntegrationTest.Adress
     {
 
         [Fact]
-        public async Task GetAdressesViaCep_WhenCepString_ReturnsPage()
+        public async Task GetAdressesViaCep_WhenCepString_ReturnsJson()
         {
             var adressApi = new ClientViaCepApi("https://viacep.com.br/", TimeSpan.FromSeconds(30));
             var result = await adressApi.GetAsyncZipCode("01311200");
+
             Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetAdressesViaCep_WhenCepString_ReturnsDataJson()
+        {
+            var adressApi = new ClientViaCepApi("https://viacep.com.br/", TimeSpan.FromSeconds(30));
+            var result = await adressApi.GetAsyncZipCode("01311200");
+
+            Assert.NotNull(result);
+            Assert.True(result.Contains("cep"));
+            Assert.True(result.Contains("logradouro"));
+            Assert.True(result.Contains("complemento"));
+            Assert.True(result.Contains("bairro"));
+            Assert.True(result.Contains("localidade"));
+            Assert.True(result.Contains("uf"));
+            Assert.True(result.Contains("unidade"));
+            Assert.True(result.Contains("ibge"));
+            Assert.True(result.Contains("gia"));
         }
 
     }
