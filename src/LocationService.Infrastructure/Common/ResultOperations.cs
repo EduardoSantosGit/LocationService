46c5494dc2debd28295ebc0056b3d95af7ccb036
EduardoSantosGit <eduardosantos058@gmail.com>
2018-07-29 20:31:14 -0300

    add class resultoperations result

---
+++ src/LocationService.Infrastructure/Common/ResultOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LocationService.Infrastructure.Common
+{
+    public class ResultOperations
+    {
+
+        public static async Task<Result<string>> ReadHttpResult(HttpResponseMessage httpResponseMessage)
+        {
+            if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+                return new Result<string>(ResultCode.OK, 
+                    HttpUtility.HtmlDecode(await httpResponseMessage.Content.ReadAsStringAsync()));
+
+            else if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return new Result<string>(ResultCode.NotFound,
+                    HttpUtility.HtmlDecode(await httpResponseMessage.Content.ReadAsStringAsync()));
+
+            else if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                return new Result<string>(ResultCode.Error,
+                    HttpUtility.HtmlDecode(await httpResponseMessage.Content.ReadAsStringAsync()));
+
+            return null;
+        }
+
+    }
+}
