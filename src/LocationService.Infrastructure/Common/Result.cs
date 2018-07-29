3fc7b8fcbf2210cdc88b20cfd1b736cf4da89e32
EduardoSantosGit <eduardosantos058@gmail.com>
2018-07-29 19:47:38 -0300

    add class generic result

---
+++ src/LocationService.Infrastructure/Common/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationService.Infrastructure.Common
+{
+
+    public enum ResultCode
+    {
+        OK = 1,
+        Error = -1
+    }
+
+    public class Result<T>
+    {
+        public ResultCode Status { get; set; }
+        public object Value { get; set; }
+
+        public T ValueType
+        {
+            get { return (T)Value; }
+        }
+
+        public Result(ResultCode status)
+        {
+            this.Status = status;
+        }
+
+        public Result(ResultCode status, object value)
+            : this(status)
+        {
+            this.Value = value;
+        }
+    }
+
+
+}
