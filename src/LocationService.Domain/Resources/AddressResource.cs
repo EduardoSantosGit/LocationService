33e708774920d3427501a9c03b506a6964275406
eduardosantosgit <eduardosantos058@gmail.com>
2018-08-01 09:11:41 -0300

    add init resource and alter return service

---
+++ src/LocationService.Domain/Resources/AddressResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationService.Domain.Resources
+{
+    public class AddressResources
+    {
+        public IEnumerable<AddressResource> Addresses { get; set; }
+    }
+
+    public class AddressResource
+    {
+
+    }
+}
---
+++ src/LocationService.Domain/Services/Addresses/SearchAddressService.cs
@@ -0,0 +1,33 @@
+﻿using LocationService.Domain.Interfaces;
+using LocationService.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationService.Domain.Services.Addresses
+{
+    public class SearchAddressService
+    {
+        public IAddressesServices _addressesServices;
+
+        public SearchAddressService(IAddressesServices addressesServices)
+        {
+            _addressesServices = addressesServices;
+        }
+
+        public async Task<Address> FindByZipCode(string zipCode)
+        {
+            var result = await _addressesServices.GetAddressesZipCode(zipCode);
+            return result.ValueType;
+        }
+        
+
+        public async Task<IEnumerable<Address>> FindByTerm(string term)
+        {
+            var result = await _addressesServices.GetAddressesTerm(term);
+            return result.ValueType;
+        }
+        
+    }
+}
