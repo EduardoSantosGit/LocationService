a0d3cc6a96fbacaa97a1954f9871411fdc420b0f
EduardoSantosGit <eduardosantos058@gmail.com>
2018-08-05 20:31:11 -0300

    add init logic slice tables address

--- src/LocationService.Infrastructure/Services/Addresses/AddressesServiceScrap.cs
+++ src/LocationService.Infrastructure/Services/Addresses/AddressesServiceScrap.cs
@@ -16,6 +16,25 @@ namespace LocationService.Infrastructure.Services.Addresses
             _scrapParser = new ScrapParser();
         }
 
+        public int CountPagesTable(string initialHtml)
+        {
+            return 0;
+        }
+
+        public List<Address> GetAddressesPageTermTemp(string html)
+        {
+            var count = CountPagesTable(html);
+
+            if(count > 1)
+            {
+                return null;
+            }
+            else
+            {
+                return GetAddressesPageTerm(html);
+            }
+        }
+
         public Address GetAddressesPageCode(string html)
         {
             var table = _scrapParser.ScrapBlockPage(html, "<table class=\"tmptabela\">", "</table>");
