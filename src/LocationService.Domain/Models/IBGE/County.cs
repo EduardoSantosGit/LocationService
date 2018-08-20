fe3cf19a6394df31d11933b450220bb4f2715646
eduardosantosgit <eduardosantos058@gmail.com>
2018-08-20 20:45:04 -0300

    add models ibge

---
+++ src/LocationService.Domain/Models/IBGE/County.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationService.Domain.Models.IBGE
+{
+    public class County
+    {
+        public string CodeIBGE { get; set; }
+        public string Gentlic { get; set; }
+        public string Mayor { get; set; }
+        public Population Population { get; set; }
+        public WorkIncome WorkIncome { get; set; }
+
+    }
+
+    public class Population
+    {
+        public string PopulationEstimated { get; set; }
+        public string PopulationLastCensus { get; set; }
+        public string DemographicDensity { get; set; }
+    }
+
+    public class WorkIncome
+    {
+        public string SalaryAverageMonth { get; set; }
+        public string PeopleBusy { get; set; }
+        public string PopulationBusyPercentage { get; set; }
+        public string PopMonthMinWage { get; set; }
+    }
+
+}
