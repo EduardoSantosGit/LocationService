b2c9e8d146f7e84ca8a9c77b1f55690c49b6f532
eduardosantosgit <eduardosantos058@gmail.com>
2018-09-03 13:56:58 -0300

    add method scrap population

--- src/LocationService.Infrastructure/Services/IBGE/IBGEProspectServiceScrap.cs
+++ src/LocationService.Infrastructure/Services/IBGE/IBGEProspectServiceScrap.cs
@@ -18,12 +18,50 @@ namespace LocationService.Infrastructure.Services.IBGE
 
         public Result<County> GetCountryPage(string html)
         {
+            var country = new County();
+
             var blockPop = _scrapParser
                 .ScrapBlockPage(html, "colspan=\"2\">População</th>", "colspan=\"2\">Trabalho e Rendimento</th>");
 
-            return null;
+            var retPop = GetPopulation(blockPop);
+            if (retPop.Status == ResultCode.OK)
+                country.Population = retPop.ValueType;
+
+            return new Result<County>(ResultCode.OK, country);
         }
-        
+
+        public Result<Population> GetPopulation(string html)
+        {
+            var tablesPop = html.SplitString("<tr _ngcontent-c2017=");
+
+            var population = new Population();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(tablesPop[1]?.ToString()))
+                    population.PopulationEstimated = _scrapParser
+                        .ScrapBlockPage(tablesPop[1], "class=\"lista__valor\" colspan=\"2\">",
+                                              "<span _ngcontent")?.Trim();
+
+                if (!string.IsNullOrEmpty(tablesPop[3]?.ToString()))
+                    population.PopulationLastCensus = _scrapParser
+                        .ScrapBlockPage(tablesPop[3], "class=\"lista__valor\" colspan=\"2\">",
+                                              "<span _ngcontent")?.Trim();
+
+                if (!string.IsNullOrEmpty(tablesPop[5]?.ToString()))
+                    population.DemographicDensity = _scrapParser
+                        .ScrapBlockPage(tablesPop[5], "class=\"lista__valor\" colspan=\"2\">",
+                                              "<span _ngcontent")?.Trim();
+            }
+            catch(Exception ex)
+            {
+                return new Result<Population>(ResultCode.Error, ex.Message);
+            }
+
+            return new Result<Population>(ResultCode.OK, population);
+        }
+
+
 
     }
 }
